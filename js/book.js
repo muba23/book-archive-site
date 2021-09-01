91500abe6c7697a9a24490b63227832de3697726
muba23 <muba2307@gmail.com>
2021-09-02 00:56:08 +0600

    html part done

--- js/book.js
+++ js/book.js
@@ -1,1 +1,6 @@
-console.log('book');
+const searchInput = document.getElementById('search-input');
+const searchBtn = document.getElementById('search-btn');
+
+searchBtn.addEventListener('click', function(){
+    console.log('search button clicked');
+})
